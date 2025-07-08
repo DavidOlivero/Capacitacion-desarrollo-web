f8701b20f5b76d6e40202bd35a64a031e80a2e66
DavidOlivero <daniolidavid32@gmail.com>
2025-07-07 20:46:59 -0500

    Objetos condicionales y estructuras de control de flujo if

---
+++ 04 - Objetos condicionales y estructuras de control de flujo/main.js
@@ -0,0 +1,42 @@
+const PEPE = {
+  nombre: 'Pepe',
+  edad: 25,
+  ciudad: 'Bogotá'
+}
+
+// Condicionales: Hacer una cosa u otra si algo es verdadero o falso
+
+// const TIENE_TARJETA = prompt('¿Tienes la tarjeta de la tienda? (si/no)')
+// const OPCIONES = {
+//   si: 'Le aplica el descuento del 20%',
+//   no: 'No le aplica el descuento del 20%',
+// }
+// console.log(OPCIONES[TIENE_TARJETA])
+
+// Estructuras de control de flujo
+// if, for, while, whiledo, foreach, forin, forof, switch
+// Condicionales: Es una estructura que nos permite realizar una acción o no dependiendo si algo en cierto o no (if, swithc)
+// Bucles: Una estructura que nos permite ejecutar un mismo código varias veces seguidas dependiendo de algo. (for, forof, forin, foreach, while, whiledo)
+
+// booleano -> false o true (verdadero o falso)
+// const tieneTitulo = prompt('¿Tiene título?')
+// const tieneExperencia = prompt('¿Tiene experiencia?')
+// const experienceYear = Number(prompt('¿Cuántos años de experiencia tienes?'))
+// // Tenga título, no es necsario que tenga experiencia, si tiene experienca tienen que tener mas de dos años de experiencia
+// if (tieneTitulo === 'si' && tieneExperencia === 'si' && experienceYear > 2 || tieneExperencia === 'no') {
+//   // Vamos a poner la acción
+//   console.log('Usted es el adecuado para el empleo')
+// } else {
+//   console.log('Usted no es el adecuado para el empleo')
+// }
+
+const EDAD_USUARIO = Number(prompt('Ingrese su edad'))
+if (EDAD_USUARIO < 18) {
+  console.log('El usuario es menor de edad')
+} else if (EDAD_USUARIO >= 18 && EDAD_USUARIO <= 39) {
+  console.log('El usuario es un adulto joven')
+} else if (EDAD_USUARIO >= 40 && EDAD_USUARIO <= 59) {
+  console.log('El usuario es adulto')
+} else {
+  console.log('El usuario es de tercera edad')
+}
